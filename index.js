4188cad3f7e9c869e2db7743dac7144f8c58af5a
Myles <mylesssalazar@gmail.com>
2020-07-14 01:30:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts() {
   var facts =["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
+  
 
 
