d2f17a7e498fc7aa66863465f5b8741176ca6168
Myles <mylesssalazar@gmail.com>
2020-07-14 01:25:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments){
-  var array = [];
+  var array = [`"John Lennon plays guitar"`];
   for (var i = 0; i < musicians.length; i++) {
     array.push(musicians[i] + "plays" + instruments[i])
   }
